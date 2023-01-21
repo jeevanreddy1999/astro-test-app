e6c4100541e4146b6f239014780bad5d06a5a3e2
Jeevan Karnati <jeevanreddy1999@gmail.com>
2023-01-21 19:42:30 +0530

    Added PostCss Plugins and nesting support.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,13 @@
+const autoprefixer = require("autoprefixer");
+const postCssPresetEnv = require("postcss-preset-env");
+const postCssImport = require("postcss-import");
+const postCssMixins = require("postcss-mixins");
+
 module.exports = {
   plugins: [
-    require("postcss-preset-env")({
-      stage: 0,
-    }),
+    autoprefixer,
+    postCssImport,
+    postCssMixins,
+    postCssPresetEnv({ stage: 0, features: { "nesting-rules": true } }),
   ],
 };
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,21 @@
-import { createSignal } from "solid-js";
+import { createSignal, createEffect, For } from "solid-js";
+
+const fruits = ["mango", "orange", "banana"];
 
 const Counter = () => {
   const [count, setCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log("count", count());
+  });
 
   return (
     <div class="layout">
       <div class="counterHead">Counter</div>
       <div class="counter">
-        <button onclick={() => setCount(count() - 1)}>-</button>
+        <button onclick={() => setCount(count() - 1)}>Remove</button>
         <div>{count()}</div>
-        <button onclick={() => setCount(count() + 1)}>+</button>
+        <button onclick={() => setCount(count() + 1)}>Add</button>
       </div>
     </div>
   );
