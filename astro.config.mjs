160eab40781856791f1f4d50e5deef13e246844c
Jeevan Karnati <jeevanreddy1999@gmail.com>
2023-01-21 14:54:05 +0530

    Added vercel support,

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  output: "server",
+  adapter: vercel(),
 });
