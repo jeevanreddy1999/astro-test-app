cc6ac171ab0b1792daaf0c0277987c9b08b9b099
Jeevan Karnati <jeevanreddy1999@gmail.com>
2023-01-22 13:01:29 +0530

    Added dark mode support.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div class="layout">
+      <input type="checkbox" id="toggleTheme" />
+      <span>toggle theme</span>
+      <br />
+      <a href="/stage">stage</a>
+      <br />
+      <a href="/">back</a>
+    </div>
+  );
+};
+
+export default Header;
